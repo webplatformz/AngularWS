5ad1d5efa678d69a8e234298862ccfec07fc02a5
Ben Millo <bmillo@gmail.com>
2015-08-23 23:16:04 +0200

    first dirty solution for prime challenge

--- app/js/primes/controllers.js
+++ app/js/primes/controllers.js
@@ -1,34 +1,52 @@
 function isPrime(n) {
-    try {
-        var k;
-        for (k = 2; k < n; k += 1) {
-            if (n % k === 0) {
-                return false;
-            }
-        }
-    } catch (err) {
-        console.error(err);
+    if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+    if (n % 2 == 0) return (n == 2);
+    if (n % 3 == 0) return (n == 3);
+    var m = Math.sqrt(n);
+    for (var i = 5; i <= m; i += 6) {
+        if (n % i == 0) return false;
+        if (n % (i + 2) == 0) return false;
     }
     return true;
 }
 
+var foundPrimes = [];
+
 function findPrime(n) {
-    var k = 1;
-    var foundPrimes = [];
+    var k;
+    if (foundPrimes.length) {
+        k = foundPrimes[foundPrimes.length - 1] + 1;
+    } else {
+        k = 1;
+    }
     while (foundPrimes.length < n) {
         if (isPrime(k)) {
             foundPrimes.push(k);
-
         }
         k += 1;
     };
-    return foundPrimes[foundPrimes.length - 1];
+    return foundPrimes[n - 1];
 }
 
 angular.module('angularws.primes')
-    .controller('primesController', ['$scope',
-        function ($scope) {
+    .controller('primesController', ['$scope', '$filter',
+        function ($scope, $filter) {
             $scope.n = 1000;
+            // use AngularJs built-in filter
+            var number = $filter('number');
+
+            function generateTableRows() {
+                var k;
+                var str = '';
+                for (k = 0; k < $scope.n; k += 1) {
+                    str += '<tr><td>index</td>';
+                    str += '<td>' + number(k + 1, 0) + '</td>';
+                    str += '<td>prime number</td>';
+                    str += '<td>' + number($scope.primes[k], 0) + '</td>';
+                    str += '<td>is prime? true</td></tr>';
+                }
+                document.getElementsByTagName('table')[0].innerHTML = str;
+            }
             $scope.find = function () {
                 console.log('computing first', $scope.n, 'primes');
                 $scope.primes = [];
@@ -39,8 +57,6 @@ angular.module('angularws.primes')
                     $scope.primes.push(prime);
                 }
                 console.timeEnd('computing primes');
-            };
+                generateTableRows();
+            }
     }])
-    .filter('isPrime', function () {
-        return isPrime;
-    })
