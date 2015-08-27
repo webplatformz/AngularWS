908dbcfa8dbfdcc107c7c4a71837e8d18da86b35
Ben Millo <bmi@zuehlke.com>
2015-08-27 16:03:39 +0200

    just check in fast:-)

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 angular.module('angularws', ['angularws.blogpost', 'ngSanitize']);
-=======
 angular.module('angularws', ['angularws.accordion']);
->>>>>>> 6779d24cf8f62e8fd0add3dc2d318e94418c76ca
 
 angular.module('angularws.accordion', []);
