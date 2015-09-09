7af28212427962683f39e0f8120bbb390aed461b
nick <nick.payne89@gmail.com>
2015-09-09 15:25:05 +0200

    first version of solution 3 - earth icon still missing

--- app/js/accordion/directives.js
+++ app/js/accordion/directives.js
@@ -1,9 +1,44 @@
 angular.module('angularws.accordion')
 
-    .directive('accordionItem', function () {
+    .directive('planetAccordionItem', function () {
         return {
             restrict: 'AE',
             replace: true,
-            templateUrl: './partials/accordionitem.html'
+            transclude: true,
+            templateUrl: './partials/accordionitem.html',
+            scope: {
+                selectedPlanet: '@planet',
+                link: '@'
+            },
+            link: function(scope) {
+                var planets = {
+                    mercury : {
+                        iconClass: 'mercury-icon',
+                        title: 'Mercury',
+                        lock: false
+                    },
+                    venus: {
+                        iconClass: 'venus-icon',
+                        title: 'Venus',
+                        lock: false
+                    },
+                    earth: {
+                        iconClass: 'earth-icon',
+                        title: 'Earth',
+                        lock: false
+                    }
+                };
+
+                scope.planet = planets[scope.selectedPlanet];
+
+                if (!scope.planet) {
+                    throw 'Unknown planet: ' + scope.selectedPlanet;
+                }
+
+                scope.togglePlanetLock = function() {
+                    scope.planet.lock = !scope.planet.lock;
+                };
+
+            }
         }
     });
