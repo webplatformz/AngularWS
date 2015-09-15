b49ed35ee8d14d0a3fbdd2db07d03e7a6a7c739e
Ben Millo <bmillo@gmail.com>
2015-09-15 22:53:13 +0200

    Changed solution 3 slightly with a more generic directive

--- app/js/accordion/controllers.js
+++
@@ -1,7 +0,0 @@
-angular.module('angularws.accordion')
-    .controller('accordionController', ['$scope',
-        function ($scope) {
-            $scope.mercuryLock = false;
-            $scope.venusLock = false;
-            $scope.earthLock = false;
-    }]);
--- app/js/accordion/directives.js
+++ app/js/accordion/directives.js
@@ -1,44 +1,22 @@
 angular.module('angularws.accordion')
 
-    .directive('planetAccordionItem', function () {
-        return {
-            restrict: 'AE',
-            replace: true,
-            transclude: true,
-            templateUrl: './partials/accordionitem.html',
-            scope: {
-                selectedPlanet: '@planet',
-                link: '@'
-            },
-            link: function(scope) {
-                var planets = {
-                    mercury : {
-                        iconClass: 'mercury-icon',
-                        title: 'Mercury',
-                        lock: false
-                    },
-                    venus: {
-                        iconClass: 'venus-icon',
-                        title: 'Venus',
-                        lock: false
-                    },
-                    earth: {
-                        iconClass: 'earth-icon',
-                        title: 'Earth',
-                        lock: false
-                    }
-                };
+.directive('accordionItem', function () {
+    return {
+        restrict: 'AE',
+        replace: true,
+        transclude: true,
+        templateUrl: './partials/accordionitem.html',
+        scope: {
+            title: '@',
+            icon: '@iconClass',
+            link: '@'
+        },
+        link: function (scope) {
+            scope.lock = false;
+            scope.togglePlanetLock = function () {
+                scope.lock = !scope.lock;
+            };
 
-                scope.planet = planets[scope.selectedPlanet];
-
-                if (!scope.planet) {
-                    throw 'Unknown planet: ' + scope.selectedPlanet;
-                }
-
-                scope.togglePlanetLock = function() {
-                    scope.planet.lock = !scope.planet.lock;
-                };
-
-            }
         }
-    });
+    }
+});
