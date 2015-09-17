654b5508d7fb8e98cf84277a19e8ae0d89b94b5b
Nick <nick.payne89@gmail.com>
2015-09-17 03:31:10 -0700

    complete the protractor test for challenge 1

--- test/webtest/soze-quiz-spec.js
+++ test/webtest/soze-quiz-spec.js
@@ -2,5 +2,13 @@ describe('keyser soze quiz', function () {
     it('should show and hide the correct answer', function () {
         browser.get('http://localhost:3000');
 
+        var hideAnswerButton = element(by.buttonText('Hide Answer'));
+        var correctAnswerButton = element(by.buttonText('1995'));
+
+        expect(hideAnswerButton.isPresent()).toBe(false);
+        correctAnswerButton.click();
+        expect(hideAnswerButton.isPresent()).toBe(true);
+        hideAnswerButton.click();
+        expect(hideAnswerButton.isPresent()).toBe(false);
     });
 });
