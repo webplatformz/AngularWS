0a7fcb2a5237c4d95acf1c1cb5c36401a9b924bd
Ben Millo <bmillo@gmail.com>
2015-09-19 01:39:18 +0200

    added local version with timeout to emulate difference

--- app/js/location/services.js
+++ app/js/location/services.js
@@ -1,4 +1,4 @@
-angular.module('location')
+/*angular.module('location')
     .factory('locationService', ['$q', '$http', '$timeout', function ($q, $http, $timeout) {
         return {
             getIPLocation: function (url) {
@@ -29,44 +29,80 @@ angular.module('location')
                 });
             }
         }
-    }]);
+    }]);*/
 
-/*angular.module('location')
-    .factory('locationService', ['$http', '$timeout', function ($http, $timeout) {
+
+angular.module('location')
+    .factory('locationService', ['$q', '$http', '$timeout', function ($q, $http, $timeout) {
         return {
-            getIPLocation: function (url, success) {
+            getIPLocation: function (url) {
+                var deferred = $q.defer();
                 $timeout(function () {
                     var response = {
-                        "as": "AS31004 Schweizerische Bundesbahnen SBB",
-                        "city": "Bern 7",
-                        "country": "Switzerland",
-                        "countryCode": "CH",
-                        "isp": "Schweizerische Bundesbahnen SBB",
-                        "lat": 46.9467,
-                        "lon": 7.44699,
-                        "org": "Schweizerische Bundesbahnen SBB",
-                        "query": "194.150.245.142",
-                        "region": "",
-                        "regionName": "Canton of Bern",
-                        "status": "success",
-                        "timezone": "Europe/Zurich",
-                        "zip": ""
+                        "data": {
+                            "as": "AS31004 Schweizerische Bundesbahnen SBB",
+                            "city": "Bern 7",
+                            "country": "Switzerland",
+                            "countryCode": "CH",
+                            "isp": "Schweizerische Bundesbahnen SBB",
+                            "lat": 46.9467,
+                            "lon": 7.44699,
+                            "org": "Schweizerische Bundesbahnen SBB",
+                            "query": "194.150.245.142",
+                            "region": "",
+                            "regionName": "Canton of Bern",
+                            "status": "success",
+                            "timezone": "Europe/Zurich",
+                            "zip": ""
+                        }
                     };
-                    success(response);
+                    deferred.resolve(response);
                 }, 2000);
+
+                return deferred.promise;
             },
-            getNS: function (url, success) {
+            getNS: function (url) {
+                var deferred = $q.defer();
                 $timeout(function () {
                     var response = {
-                        "name": "sbb.ch.",
-                        "type": "NS",
-                        "class": "IN",
-                        "ttl": 11168,
-                        "rdlength": 6,
-                        "rdata": "ns3.sbb.ch."
+                        "data": {
+                            "authority": [
+                                {
+                                    "name": "sbb.ch.",
+                                    "type": "NS",
+                                    "class": "IN",
+                                    "ttl": 5928,
+                                    "rdlength": 6,
+                                    "rdata": "ns3.sbb.ch."
+                            }
+                        ]
+                        }
                     };
-                    success(response);
+                    deferred.resolve(response);
                 }, 2000);
+
+                return deferred.promise;
+            },
+            checkSyntax: function (url) {
+                var deferred = $q.defer();
+                var domainRegex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+                var regex = new RegExp(domainRegex);
+
+                if (url.match(regex)) {
+                    deferred.resolve();
+                } else {
+                    deferred.reject();
+                }
+                return deferred.promise;
+            },
+            checkAndLoadLocation: function (locationObject, url) {
+                var that = this;
+                var tempLocation = {};
+                return that.checkSyntax(url).then(function (isValidUrl) {
+                    return $q.all([
+                        that.getIPLocation(url), that.getNS(url)
+                    ])
+                });
             }
         }
-    }]);*/
+    }]);
