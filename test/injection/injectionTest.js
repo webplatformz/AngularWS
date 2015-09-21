ae7c9e8e8135c82de044a4d4652e0b839d5eeb6a
nick <nick.payne89@gmail.com>
2015-09-21 16:52:33 +0200

    update readme, make test a little more readable

--- test/injection/injectionTest.js
+++ test/injection/injectionTest.js
@@ -1,21 +1,24 @@
 describe('angularws-app', function () {
-    var scope,
-        controller;
+    var scope, controller;
+
     beforeEach(function () {
         module('injection');
     });
+
     describe('injectionController', function () {
+
         beforeEach(inject(function ($rootScope, $controller) {
             scope = $rootScope.$new();
-
             controller = $controller('injectionController', {
                 '$scope': scope
             });
         }));
+
         it('should set greeting and name', function () {
             expect(scope.greeting).toBe('Hello everybody');
             expect(scope.name).toBe('Keyser Söze');
         });
+
         it('should set birthdate and default setting', function () {
             expect(scope.showAnswer).toBe(false);
             expect(scope.year).toBe('1995');
